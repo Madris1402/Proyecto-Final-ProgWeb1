4af6927e2ff918a7c2a344362b0112e6f3bc2724
Madris1402 <ricmad142@gmail.com>
2023-11-25 13:55:08 -0600

    Ajustes del Update

--- update.php
+++ update.php
@@ -6,11 +6,11 @@
   $pass = $_POST["pass"];
   $cellnum = isset($_POST["cellnum"]) ? intval($_POST["cellnum"]) : 0;
   $email = $_POST["email"];
-  $sql = "update usuarios set nombre='$nombre', pass='$pass', cellnum='$cellnum', email='$email' where id=$id";
+  $permisos = $_POST["permisos"];
+  $sql = "update usuarios set nombre='$nombre', pass='$pass', cellnum='$cellnum', email='$email', permisos='$permisos' where id=$id";
   $result = $conexion->query($sql);
   $conexion->close();
 
   header("Location: home.php");
   
-?>
 ?>
