530f7221e8fa7d85cdf2947feb7c9dc265a9ae70
Ricardo Madrigal U <114124131+Madris1402@users.noreply.github.com>
2023-11-25 11:56:58 -0600

    Página de Inicio

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Proyecto Final ProgWeb1</title>
+</head>
+<body>
+    <div class='header'>
+        <h1>Proyecto Final Programación Web 1</h1>
+        <h2>Por: Ricardo Madrigal Urencio</h2>
+    </div>
+    <div class='opciones'>
+        <a class="button" href="registro.php">Registrarse</a> <br> <br>
+        <a class="button" href="sesion.php">Iniciar Sesion</a> <br> <br>
+        <a class="button" href="list.php">Ver Usuarios Registrados</a> <br> 
+    </div>
+</body>
+</html>
