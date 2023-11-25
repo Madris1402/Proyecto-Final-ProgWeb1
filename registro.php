e03e05e8efd99475f47bd6db1f55a9731492fa61
Madris1402 <ricmad142@gmail.com>
2023-11-25 12:14:57 -0600

    Mejoras de Interfaz

--- registro.php
+++ registro.php
@@ -15,15 +15,15 @@
 
         <form class="temp" action="create.php" method="POST">
           <label for="nombre"> Nombre:</label> <br>
-          <input class="input" type="text" id="nombre" name="nombre"> <br>
+          <input class="input" type="text" id="nombre" required placeholder name="nombre"> <br>
           <label for="usuario"> Usuario:</label> <br>
-          <input class="input" type="text" id="usuario" name="usuario"> <br>
+          <input class="input" type="text" id="usuario" required placeholder name="usuario"> <br>
           <label for="pass"> Contraseña:</label> <br>
-          <input class="input" type="text" id="pass" name="pass"> <br>
+          <input class="input" type="text" id="pass" required placeholder name="pass"> <br>
           <label for="cellnum"> Número de Teléfono:</label> <br>
-          <input class="input" type="number" id="cellnum" name="cellnum"> <br>
+          <input class="input" type="number" id="cellnum" required placeholder name="cellnum"> <br>
           <label for="email"> Correo Electrónico:</label> <br>
-          <input class="input" type="text" id="email" name="email"> <br>
+          <input class="input" type="text" id="email" required placeholder name="email"> <br>
           <button type="submit" class="btn btn-primary">Registrar</button>
         </form>
 </div>
